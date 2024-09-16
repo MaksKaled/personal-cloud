53f86ed7468a01b38b1e994a58a65a0f62034036
MaksKaled <kaledinmaksim11@gmail.com>
2024-09-16 13:17:51 +0300

    добавлен функционал обработки дублирующих названий: dir,dir(1),dir(2)...

--- server/models/Directory.js
+++ server/models/Directory.js
@@ -4,6 +4,7 @@ const directorySchema = new mongoose.Schema({
     name:{type:String, required:true},
     path:{type:String, required:true},
     isDirectory:{type:Boolean, default:true},
+    index:{type:Number,default:0}
 })
 
 const Directory = mongoose.model('Directory',directorySchema)
